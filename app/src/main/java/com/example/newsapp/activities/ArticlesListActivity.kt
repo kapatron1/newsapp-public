3ec31fac5408acb2a1eb684d0570089eb555bf1e
kapatron1 <darkmt4@gmail.com>
2024-04-20 18:24:48 +0100

    add swipe up feature to refresh and fallback in case of no internet show empty list message

--- app/src/main/java/com/example/newsapp/activities/ArticlesListActivity.kt
+++ app/src/main/java/com/example/newsapp/activities/ArticlesListActivity.kt
@@ -1,28 +1,57 @@
 package com.example.newsapp.activities
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.newsapp.BuildConfig
 import com.example.newsapp.R
 import com.example.newsapp.adapters.ArticleAdapter
 import com.example.newsapp.repository.NewsRepository
+import com.example.newsapp.utils.Utils
 import com.example.newsapp.viewmodel.ArticlesViewModel
 import java.util.Locale
 
 class ArticlesListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ArticlesViewModel
+    private lateinit var lvArticles :ListView
+    private lateinit var tvNoArticles :TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_articles_list)
-
-        initViewModel()
-        getArticles()
-        initObservers()
-
         title =  BuildConfig.SOURCE.uppercase(Locale.getDefault())
+        lvArticles = findViewById(R.id.lvArticles)
+        tvNoArticles = findViewById(R.id.tvNoArticles)
+
+        updateListView()
+
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.swipe)
+        swipeRefreshLayout.setOnRefreshListener {
+            updateListView()
+            Handler().postDelayed({
+                swipeRefreshLayout.isRefreshing = false
+            }, 2000)
+        }
+    }
+
+    private fun updateListView(){
+        if(Utils.isOnline(this)) {
+            initViewModel()
+            getArticles()
+            initObservers()
+            lvArticles.visibility = View.VISIBLE
+            tvNoArticles.visibility = View.GONE
+        } else {
+            Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show()
+            lvArticles.visibility = View.GONE
+            tvNoArticles.visibility = View.VISIBLE
+        }
     }
 
     private fun initViewModel() {
@@ -31,7 +60,7 @@ class ArticlesListActivity : AppCompatActivity() {
         )
     }
 
-    private fun getArticles() {
+    private fun getArticles(){
         viewModel.getArticles()
     }
 
---
+++ app/src/main/java/com/example/newsapp/utils/Utils.kt
@@ -0,0 +1,25 @@
+package com.example.newsapp.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+class Utils {
+
+    companion object {
+        fun isOnline(context: Context): Boolean {
+            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+            val n = cm.activeNetwork
+            if (n != null) {
+                val nc = cm.getNetworkCapabilities(n)
+                //It will check for both wifi and cellular network
+                return nc!!.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(
+                    NetworkCapabilities.TRANSPORT_WIFI
+                )
+            }
+            return false
+        }
+    }
+}
